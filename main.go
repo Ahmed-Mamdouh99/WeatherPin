915ef81d9b8c29d0fdf39ee628be4b0b51b7b4e6
Ahmed Khalifa <medoduh@gmail.com>
2018-11-23 21:52:41 +0200

    Added graceful stop function and rearranging some lines

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 type Config struct {
@@ -19,18 +21,17 @@ type Config struct {
 	RedisPasswd string
 	RedisDB     int
 	RedisHost   string
-	//Here maps
+	// Here maps
 	HereID      string
 	HereAppCode string
-	//OWM OpenWeatherMap
+	// OWM OpenWeatherMap
 	OWMKey string
 }
 
 var (
-	//Config form the config file
+	// Config form the config file
 	appConfig = readConfig()
-	//readConfig()
-	//Redis client
+	// Redis client
 	redis_client = redis.NewClient(&redis.Options{
 		Addr:     appConfig.RedisHost + ":" + appConfig.RedisPort,
 		Password: appConfig.RedisPasswd,
@@ -38,40 +39,27 @@ var (
 	})
 )
 
-//Reads the file config.yml
+// Reads the file config.yml
 func readConfig() Config {
+	var config Config
 	url := "conf.yml"
 	pwd, _ := os.Getwd()
+	// Load the file
 	yamlFile, err := ioutil.ReadFile(pwd + url)
-	var config Config
-
 	if err != nil {
-		log.Fatal("Could not read config file: ", err)
-		log.Println("Falling back to default config")
-		getDefaultConfig(&config)
-		return config
+		bail(err)
 	}
-
-	err = yaml.Unmarshal(yamlFile, &config)
+	// parse yaml file
+	yaml.Unmarshal(yamlFile, &config)
 	if err != nil {
-		log.Fatalf("Unmarshal: %v", err)
-		log.Println("Falling back to default config.")
-		getDefaultConfig(&config)
+		bail(err)
 	}
 	return config
 }
 
-//Sets the config to the default
-func getDefaultConfig(c *Config) {
-	*c = Config{
-		"", "8080", "6379", "", 0,
-		"redis-service", "ivpuD5BHEfdq49qdXWIS",
-		"jrVubtlb8B0giad6hzo4YA", "8998549a7a2993add254b22a8c84cc8c"}
-}
-
-//This function should handle the index page
+// This function should handle the index page
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	//TODO - Handle requests from the index page.
+	// TODO - Handle requests from the index page.
 	if r.Method == "POST" {
 	}
 	if r.Method == "GET" {
@@ -80,11 +68,37 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func redis_handler(w http.ResponseWriter, r *http.Request) {
-	//TODO - Handle requests from the front end... Maybe?
+	// TODO - Handle requests from the front end... Maybe?
+}
+
+func bail(err error){
+	log.Printf("Error: %v.", err)
+	log.Fatal("Bailing out. You're on your own now :(")
+}
+
+func gracefulStop(){
+	//VV TODO - Clean up goes here VV
 }
 
 func main() {
+	// Listening on the web port
 	defer http.ListenAndServe(":"+appConfig.WebPort, nil)
+	// Making sure the config file was read
+	if appConfig.WebPort == ""{
+		log.Fatal("Weatherpin-Main: Failed to start the app.")
+	}
+	// Graceful exit
+	var gracefulStop = make(chan os.Signal)
+	signal.Notify(gracefulStop, syscall.SIGTERM)
+	signal.Notify(gracefulStop, syscall.SIGINT)
+	go func(){
+		sig := <-gracefulStop
+		fmt.Printf("Caught sig: %+v.", sig)
+		gracefulStop()
+		fmt.Println("Good night!")
+		os.Exit(0)
+	}()
+	// Handling the calls to the index
 	http.HandleFunc("/", indexHandler)
 	log.Println("Serving port: ", appConfig.WebPort)
 }
