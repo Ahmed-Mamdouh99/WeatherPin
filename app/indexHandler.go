51bcc852c6de9a83200ab7028b3fcc2f4b241344
Yassin Chaddad <yassinchaddad@users.noreply.github.com>
2018-11-30 05:06:30 +0200

    Add backend request parsing and response sending

--- app/indexHandler.go
+++ app/indexHandler.go
@@ -1,36 +1,64 @@
 package main
 
 import (
-  "net/http"
-  "html/template"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
 )
 
+type WeatherData struct {
+	Name     string
+	CityName string
+}
+
 // This function should handle the index page
-func IndexHandler(w http.ResponseWriter, r *http.Request){
-  	// TODO - Handle requests from the index page
-  	if r.Method == "POST" {
-      //TBD
-  	}
-  	if r.Method == "GET" {
-      //TBD
-  	}
-    //Render index template
-  	// Define parameters to pass to the index template
-  	p := struct{
-  		App_id string
-  		App_code string
-  	}{
-  		App_id: AppConfig.HereID,
-  		App_code: AppConfig.HereAppCode,
-  	}
-  	// Create template
-  	t, err := template.ParseFiles("/static/index.html")
-  	if err != nil{
-  		Bail(err)
-  	}
-  	// Execute template
-  	err = t.Execute(w, p)
-  	if err != nil{
-  		Bail(err)
-  	}
-  }
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	// TODO - Handle requests from the index page
+	if r.Method == "POST" {
+		//TBD
+		lan, _ := strconv.Atoi(r.FormValue("lan"))
+		lon, _ := strconv.Atoi(r.FormValue("lon"))
+		fmt.Println("Receive ajax post data string ", lan, " ", lon)
+		url := fmt.Sprintf("http://api.openweathermap.org/data/2.5/weather?lat=%d&lon=%d&APPID=637aa79319844d254e33bad285e23e82", lan, lon)
+
+		res, err := http.Get(url)
+		if err != nil {
+			Bail(err)
+		}
+
+		body, readErr := ioutil.ReadAll(res.Body)
+		if readErr != nil {
+			log.Fatal(readErr)
+		}
+		fmt.Println(body)
+
+		w.Header().Set("Content-Type", res.Header.Get("Content-Type"))
+		w.Write(body)
+		res.Body.Close()
+	}
+	if r.Method == "GET" {
+		//TBD
+		p := struct {
+			App_id   string
+			App_code string
+		}{
+			App_id:   AppConfig.HereID,
+			App_code: AppConfig.HereAppCode,
+		}
+		// Create template
+		t, err := template.ParseFiles("/static/index.html")
+		if err != nil {
+			Bail(err)
+		}
+		// Execute template
+		err = t.Execute(w, p)
+		if err != nil {
+			Bail(err)
+		}
+	}
+	//Render index template
+	// Define parameters to pass to the index template
+}
